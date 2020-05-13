40e6df74a888bd4cb3593ac96770ff6b6db32530
LENA EIVY <applena@gmail.com>
2020-05-13 13:50:39 -0700

    day 13 updated

---
+++ class-13/demo/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var allCats = [];
+
+function Cat(name, age, loud, catNip){
+  this.name = name;
+  this.age = age;
+  this.isLoud = loud;
+  this.likeCatNip = catNip;
+  allCats.push(this);
+}
+
+new Cat('Spaceghost', 10, true, true);
+new Cat('Harry Winston', 10, true, true);
+new Cat('Lucipurr', 4, true, true);
+new Cat('Ethel', 4, false, true);
+new Cat('Sophie', 2, true, true);
+new Cat('Orie', 2, false, true);
+
+console.log('my original cats array', allCats)
+
+// Step 1. turn the thing you want to save in Local Storage into JSON
+  // JSON.stringify()
+
+var stringifiedCats = JSON.stringify(allCats);
+console.log('this is the JSON for the all Cats array', stringifiedCats);
+
+// Step 2. set the item into Local Storage. Give it any key you want as a string and set the value as the JSON
+
+localStorage.setItem('cats', stringifiedCats);
+
+// Step 3. to get something out of local storage, we are going to get an item
+
+var catsFromLocalStorage = localStorage.getItem('cats');
+console.log('this is my cats from Local Storage', catsFromLocalStorage);
+
+// Step 4. turn it back into javascript
+
+var catsTurnedBackIntoJavaScript = JSON.parse(catsFromLocalStorage);
+console.log('my parsed cats', catsTurnedBackIntoJavaScript);
+
+// the returned cats array is now a normal array of objects - it has lost its connection to the constructor
